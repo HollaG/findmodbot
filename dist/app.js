d4edb2329c20e489386d4481d5072a637298a1cf
Marcus Soh <marcussoh38@gmail.com>
2022-10-24 00:52:21 +0800

    finish ver1

--- dist/app.js
+++ dist/app.js
@@ -7,63 +7,70 @@ const telegraf_1 = require("telegraf");
 console.log("Running file");
 const dotenv_1 = __importDefault(require("dotenv"));
 const node_fetch_1 = __importDefault(require("node-fetch"));
+const date_fns_1 = require("date-fns");
 dotenv_1.default.config();
 const bot = new telegraf_1.Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => {
-    ctx.reply("Hello " + ctx.from.first_name + "!");
-});
-bot.help((ctx) => {
-    ctx.reply("Send /start to receive a greeting");
-    ctx.reply("Send /keyboard to receive a message with a keyboard");
-    ctx.reply("Send /quit to stop the bot");
-});
-bot.command("quit", (ctx) => {
-    // Explicit usage
-    ctx.telegram.leaveChat(ctx.message.chat.id);
-    // Context shortcut
-    ctx.leaveChat();
-});
-bot.command("keyboard", (ctx) => {
-    ctx.reply("Keyboard", telegraf_1.Markup.inlineKeyboard([
-        telegraf_1.Markup.button.callback("First option", "first"),
-        telegraf_1.Markup.button.callback("Second option", "second"),
-    ]));
-});
-bot.on("text", (ctx) => {
-    ctx.reply("You choose the " +
-        (ctx.message.text === "first" ? "First" : "Second") +
-        " Option!");
+    ctx.reply(`This bot only works in inline mode. \nType @${ctx.me} [your module search term] in any chat to use it. \n\nFor example, to search for all modules starting with GEA, type \n@${ctx.me} GEA`);
 });
 bot.on("inline_query", async (ctx) => {
-    const query = ctx.inlineQuery.query.trim().toUpperCase();
-    if (query.length < 2)
-        return;
-    const moduleListResponse = await (0, node_fetch_1.default)("https://api.nusmods.com/v2/2022-2023/moduleList.json");
-    const moduleList = await moduleListResponse.json();
-    const filteredList = moduleList.filter((module) => module.moduleCode.toUpperCase().includes(query) || module.title.toUpperCase().includes(query));
-    // lowest moduleCode number first
-    const sortedList = filteredList.sort((a, b) => {
-        // remove all non-numeric characters
-        const aCode = a.moduleCode.replace(/\D/g, "");
-        const bCode = b.moduleCode.replace(/\D/g, "");
-        return parseInt(aCode) - parseInt(bCode);
-    });
-    console.log(sortedList);
-    const result = sortedList.map((module) => {
-        return {
-            type: "article",
-            id: module.moduleCode,
-            title: `${module.moduleCode} - ${module.title}`,
-            input_message_content: {
-                message_text: `${module.moduleCode} - ${module.title}`
-            }
-        };
-    });
-    // limit results to 50.
-    const trimmedResults = result.slice(0, 50);
-    ctx.answerInlineQuery(trimmedResults, {
-        cache_time: 0 // to change
-    });
+    try {
+        const query = ctx.inlineQuery.query.trim().toUpperCase();
+        if (query.length < 2)
+            return;
+        const moduleListResponse = await (0, node_fetch_1.default)("https://api.nusmods.com/v2/2022-2023/moduleInfo.json");
+        const moduleList = await moduleListResponse.json();
+        const filteredList = moduleList.filter((module) => module.moduleCode.toUpperCase().includes(query) ||
+            module.title.toUpperCase().includes(query));
+        // lowest moduleCode number first.
+        // Note that we don't have to sort by alphabetical module code because it's
+        // already sorted alphabetically from NUSMods.
+        const sortedList = filteredList.sort((a, b) => {
+            // remove all non-numeric characters
+            const aCode = a.moduleCode.replace(/\D/g, "");
+            const bCode = b.moduleCode.replace(/\D/g, "");
+            return parseInt(aCode) - parseInt(bCode);
+        });
+        const result = sortedList.map((module) => {
+            return {
+                type: "article",
+                id: module.moduleCode,
+                title: `${module.moduleCode} - ${module.title}`,
+                input_message_content: {
+                    message_text: buildMessage(module),
+                    parse_mode: "HTML",
+                },
+            };
+        });
+        // limit results to 50.
+        const trimmedResults = result.slice(0, 50);
+        ctx.answerInlineQuery(trimmedResults, {
+            cache_time: 0,
+        });
+    }
+    catch (e) {
+        console.log(e);
+    }
 });
+function buildMessage(module) {
+    let msg = `<b><u><a href='https://nusmods.com/modules/${module.moduleCode}'>${module.moduleCode} ${module.title}</a></u></b>\n`;
+    msg += `${module.department}, ${module.faculty}\n`;
+    msg += `${module.moduleCredit} MC ${module.attributes?.su ? "(Eligible for S/U)" : "(Ineligible for S/U)"}\n\n`;
+    let semestersWithExams = module.semesterData.filter((sem) => sem.examDate);
+    if (semestersWithExams.length) {
+        msg += semestersWithExams
+            .map((sem) => `Sem ${sem.semester} Exam: ${(0, date_fns_1.format)(new Date(sem.examDate || new Date()), "dd MMM yyyy h:mm a")} ${sem.examDuration && `(${sem.examDuration / 60} hrs)\n`}`)
+            .join("");
+        msg += "\n";
+    }
+    else {
+        msg += `No exams for this module.\n\n`;
+    }
+    msg += `${module.description ? trim(module.description, 256) : ""}`;
+    return msg;
+}
+function trim(str, length) {
+    return str.length > length ? str.substring(0, length - 3) + "..." : str;
+}
 bot.launch();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -4,88 +4,103 @@ console.log("Running file");
 
 import dotenv from "dotenv";
 import fetch from "node-fetch";
-import { ModuleCondensed } from "./types/nusmods";
+import { ModuleCondensed, ModuleInformation } from "./types/nusmods";
 import { InlineQueryResultArticle } from "telegraf/typings/core/types/typegram";
+
+import { format } from "date-fns";
 dotenv.config();
 const bot: Telegraf<Context<Update>> = new Telegraf(
     process.env.BOT_TOKEN as string
 );
 
 bot.start((ctx) => {
-    ctx.reply("Hello " + ctx.from.first_name + "!");
-});
-bot.help((ctx) => {
-    ctx.reply("Send /start to receive a greeting");
-    ctx.reply("Send /keyboard to receive a message with a keyboard");
-    ctx.reply("Send /quit to stop the bot");
-});
-bot.command("quit", (ctx) => {
-    // Explicit usage
-    ctx.telegram.leaveChat(ctx.message.chat.id);
-    // Context shortcut
-    ctx.leaveChat();
-});
-bot.command("keyboard", (ctx) => {
     ctx.reply(
-        "Keyboard",
-        Markup.inlineKeyboard([
-            Markup.button.callback("First option", "first"),
-            Markup.button.callback("Second option", "second"),
-        ])
-    );
-});
-bot.on("text", (ctx) => {
-    ctx.reply(
-        "You choose the " +
-            (ctx.message.text === "first" ? "First" : "Second") +
-            " Option!"
+        `This bot only works in inline mode. \nType @${ctx.me} [your module search term] in any chat to use it. \n\nFor example, to search for all modules containing 'GEA', type \n@${ctx.me} GEA`
     );
 });
 
 bot.on("inline_query", async (ctx) => {
-    const query = ctx.inlineQuery.query.trim().toUpperCase();
-    if (query.length < 2) return;
+    try {
+        const query = ctx.inlineQuery.query.trim().toUpperCase();
+        if (query.length < 2) return;
 
-    const moduleListResponse = await fetch(
-        "https://api.nusmods.com/v2/2022-2023/moduleList.json"
-    );
-    const moduleList: ModuleCondensed[] = await moduleListResponse.json();
+        const moduleListResponse = await fetch(
+            "https://api.nusmods.com/v2/2022-2023/moduleInfo.json"
+        );
+        const moduleList: ModuleInformation[] = await moduleListResponse.json();
 
-    const filteredList = moduleList.filter(
-        (module) =>
-            module.moduleCode.toUpperCase().includes(query) || module.title.toUpperCase().includes(query)
-    );
+        const filteredList = moduleList.filter(
+            (module) =>
+                module.moduleCode.toUpperCase().includes(query) ||
+                module.title.toUpperCase().includes(query)
+        );
 
-    // lowest moduleCode number first.
-    // Note that we don't have to sort by alphabetical module code because it's
-    // already sorted alphabetically from NUSMods.
-    const sortedList = filteredList.sort((a, b) => {
-        // remove all non-numeric characters
-        const aCode = a.moduleCode.replace(/\D/g, "");
-        const bCode = b.moduleCode.replace(/\D/g, "");
-        return parseInt(aCode) - parseInt(bCode);
-    });
+        // lowest moduleCode number first.
+        // Note that we don't have to sort by alphabetical module code because it's
+        // already sorted alphabetically from NUSMods.
+        const sortedList = filteredList.sort((a, b) => {
+            // remove all non-numeric characters
+            const aCode = a.moduleCode.replace(/\D/g, "");
+            const bCode = b.moduleCode.replace(/\D/g, "");
+            return parseInt(aCode) - parseInt(bCode);
+        });
 
-    console.log(sortedList);
+        const result: InlineQueryResultArticle[] = sortedList.map((module) => {
+            return {
+                type: "article",
+                id: module.moduleCode,
+                title: `${module.moduleCode} - ${module.title}`,
+                input_message_content: {
+                    message_text: buildMessage(module),
+                    parse_mode: "HTML",
+                },
+            };
+        });
 
-    const result:InlineQueryResultArticle[] = sortedList.map((module) => {
-        return {
-            type: "article",
-            id: module.moduleCode,
-            title: `${module.moduleCode} - ${module.title}`,
-            input_message_content: {
-                message_text: `Placeholder text`
-            }
-        }
-    })
+        // limit results to 50.
+        const trimmedResults = result.slice(0, 50);
 
-    // limit results to 50.
-    const trimmedResults = result.slice(0, 50);
-
-    ctx.answerInlineQuery(trimmedResults, {
-        cache_time: 0 // to change
-    })
-
+        ctx.answerInlineQuery(trimmedResults, {
+            cache_time: 0,
+        });
+    } catch (e) {
+        console.log(e);
+    }
 });
+
+function buildMessage(module: ModuleInformation) {
+    let msg = `<b><u><a href='https://nusmods.com/modules/${module.moduleCode}'>${module.moduleCode} ${module.title}</a></u></b>\n`;
+
+    msg += `${module.department}, ${module.faculty}\n`;
+    msg += `${module.moduleCredit} MC ${
+        module.attributes?.su ? "(Eligible for S/U)" : "(Ineligible for S/U)"
+    }\n\n`;
+
+    let semestersWithExams = module.semesterData.filter((sem) => sem.examDate);
+    if (semestersWithExams.length) {
+        msg += semestersWithExams
+            .map(
+                (sem) =>
+                    `Sem ${sem.semester} Exam: ${format(
+                        new Date(sem.examDate || new Date()),
+                        "dd MMM yyyy h:mm a"
+                    )} ${
+                        sem.examDuration && `(${sem.examDuration / 60} hrs)\n`
+                    }`
+            )
+            .join("");
+        msg += "\n";
+    } else {
+        msg += `No exams for this module.\n\n`;
+    }
+
+    msg += `${module.description ? trim(module.description, 256) : ""}`;
+
+    return msg;
+}
+
+function trim(str: string, length: number) {
+    return str.length > length ? str.substring(0, length - 3) + "..." : str;
+}
 
 bot.launch();
